47d512dcb044f6f9c48c6a3a48480e454946d389
Amrit Rai <iamamrit27@gmail.com>
2024-08-09 19:20:05 +0530

    add auth

--- utils/supabaseClient.ts
+++
@@ -1,6 +0,0 @@
-import { createClient, SupabaseClient } from '@supabase/supabase-js'
-
-const supabaseUrl = process.env.SUPABASE_URL!
-const supabaseKey = process.env.SUPABASE_KEY!
-
-export const supabase: SupabaseClient = createClient(supabaseUrl, supabaseKey)
