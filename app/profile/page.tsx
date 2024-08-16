3658bc3f0c84c4a895d66d881f7c8c415acdacef
Amrit Rai <iamamrit27@gmail.com>
2024-08-16 18:59:56 +0530

    chore: Update routing paths for authentication and landing page

--- app/profile/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const page = () => {
-  return (
-    <div>damn, you made it. fortunately, this is still a work in progress so you cannot roast me yet :)</div>
-  )
-}
-
-export default page
