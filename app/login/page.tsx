de5677a18dcf56f85043f275e5302dd110603105
Amrit Rai <iamamrit27@gmail.com>
2024-08-17 11:54:22 +0530

    chore: Update routing paths for authentication and landing page

---
+++ app/login/page.tsx
@@ -0,0 +1,15 @@
+import { Button } from "@/components/ui/button"
+import { signIn } from "@/auth"
+
+export default function SignIn() {
+  return (
+    <form
+      action={async () => {
+        "use server"
+        await signIn("github")
+      }}
+    className="flex items-center justify-center min-h-screen">
+      <Button type="submit">Signin with GitHub</Button>
+    </form>
+  )
+} 
---
+++ auth.ts
@@ -0,0 +1,10 @@
+import NextAuth from "next-auth"
+import GitHub from "next-auth/providers/github"
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { PrismaClient } from "@prisma/client"
+ 
+const prisma = new PrismaClient()
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [GitHub],
+  adapter: PrismaAdapter(prisma),
+})
