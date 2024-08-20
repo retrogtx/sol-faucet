60fb3362f5392f94873c374931a1074cb36e7c4d
Amrit Rai <iamamrit27@gmail.com>
2024-08-20 21:39:28 +0530

    feat: Add TestNet switch and Solana address input to Faucet component

--- components/ui/faucet.tsx
+++ components/ui/faucet.tsx
@@ -1,8 +1,27 @@
-import { Button } from "./button"
+'use client'
+
+import React, { useState } from "react"
+import { Input } from "./input"
+import { Switch } from "@/components/ui/switch" // Adjust the import path as necessary
 
 const Faucet = () => {
+  const [address, setAddress] = useState<string>("")
+  const [isTestNet, setIsTestNet] = useState<boolean>(false)
+
+  const handleSwitchChange = (checked: boolean) => {
+    setIsTestNet(checked)
+  }
+
   return (
-    <Button>Faucet</Button>
+    <>
+      <div className="flex flex items-center justify-between rounded-lg border p-3 shadow-sm">
+        <div className="space-y-0.5">
+          <label className="form-label">TestNet</label>
+        </div>
+        <Switch checked={isTestNet} onCheckedChange={handleSwitchChange} />
+      </div>
+      <Input placeholder="enter solana address" value={address} onChange={(e) => setAddress(e.target.value)} />
+    </>
   )
 }
 
