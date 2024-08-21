a4def5d316cb1e383bf5be9c7d647f728e3fae57
Amrit Rai <iamamrit27@gmail.com>
2024-08-21 20:53:12 +0530

    feat: Add lazy loading for images and update Solana dependency

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Faucet from "@/components/ui/faucet";
 export default function Home() {
  return (
   <div>
-    Sol Faucet
     <Faucet />
   </div>
  )
--- components/ui/faucet.tsx
+++ components/ui/faucet.tsx
@@ -1,16 +1,34 @@
 'use client'
 
-import React, { useState } from "react"
+import React, { useEffect, useState } from "react"
 import { Input } from "./input"
 import { Switch } from "@/components/ui/switch"
+import { PublicKey } from "@solana/web3.js"
+import { Button } from "./button"
 
 const Faucet = () => {
   const [address, setAddress] = useState<string>("")
   const [isTestNet, setIsTestNet] = useState<boolean>(false)
-
+  const [isValid, setIsValid] = useState<boolean>(false)
   const handleSwitchChange = (checked: boolean) => {
     setIsTestNet(checked)
   }
+
+  const validateAddress = (address: string) => {
+    let publicKey: PublicKey;
+    try {
+      publicKey = new PublicKey(address)
+      return PublicKey.isOnCurve(publicKey.toBytes())
+    } catch (err) {
+      console.log(err);
+      return false
+    }
+  }
+
+  useEffect(() => {
+    const isValid = validateAddress(address);
+    setIsValid(isValid);
+  }, [address])
 
   return (
     <div className="flex items-center justify-center min-h-screen">
@@ -24,6 +42,7 @@ const Faucet = () => {
           <p><strong>Address:</strong> {address}</p>
           <p><strong>TestNet:</strong> {isTestNet ? "Enabled" : "Disabled"}</p>
         </div>
+        <Button disabled={!isValid} onClick={() => console.log("Faucet request sent")}>Request</Button>
       </div>
     </div>
   )
